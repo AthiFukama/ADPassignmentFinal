a905cb073c93fbab11f0584e1177357b94583e39
Athi <218328591@mycput.ac.za>
2021-04-01 21:54:30 +0200

    First commit

---
+++ src/main/java/za/ac/cput/ADPassignmentFinal/EmployeeTest.java
@@ -0,0 +1,55 @@
+package za.ac.cput.ADPassignmentFinal;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static java.time.Duration.ofMinutes;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class EmployeeTest {
+    private Employee employee1;
+    private Employee employee2;
+    private Employee employee3;
+
+    @BeforeEach
+    void setUp(){
+        employee1 = new Employee();
+        employee2 = new Employee();
+        employee3 = employee1;
+
+    }
+    @Test
+     void testEquality(){
+        assertEquals(employee1,employee3);
+    }
+    @Test
+    void testIdentity(){
+
+        assertSame(employee1,employee3);
+
+    }
+    @Test
+    void testFail(){
+        Assertions.fail("fail test");
+
+    }
+    @Test
+    void testTimeouts(){
+        assertTimeout(Duration.ofMillis(10), () -> Thread.sleep(100));
+    }
+    @Test
+    void timeoutNotExceeded() {
+        assertTimeout(ofMinutes(2), () -> {
+        });
+    }
+    @Test
+    void disableTest(){
+        Assumptions.assumeFalse(employee1 == employee2);
+
+    }
+    }
